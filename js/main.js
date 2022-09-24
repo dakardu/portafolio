aa12c7079408a0177d120ee2e6e5a41dbe1f1506
Dakardu <unodago@gmail.com>
2022-09-24 19:10:45 +0200

    Actualizacion de todos los archivos HTML + CSS y JS

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ const burger =document.querySelector('.burger')
 const arrayImages = ['img/desarrollo.jpg', 'img/pc.jpg', 'img/laweb.jpg', 'img/seguridad.jpeg', 'img/web.jpg', 'img/notebook.jpg']
 const btnProyuno = document.querySelectorAll('.container__button--proyuno')
 const btnProydos = document.querySelectorAll('.container__button--proydos')
+const btnOcultarNetx = document.querySelector('.container__adelante')
+const btnOcultarBack = document.querySelector('.container__atras')
+const body = document.querySelector('.body')
+console.log(body)
+
 let contador = 0
 let angle = 0
 
@@ -26,18 +31,18 @@ function carrousel(contenedor){
 
         if(tgt == atras){
             if(contador > 0){
-                img.style.backgroundImage = `linear-gradient(rgb(49, 36, 80,0.363), rgba(0, 0, 0, 0.486)), url(${arrayImages[contador - 1]})`
+                img.style.backgroundImage = `linear-gradient(rgb(64 125 197 / 14%), rgb(27 21 21 / 80%)), url(${arrayImages[contador - 1]})`
                 contador--
             }else{
-                img.style.backgroundImage = `linear-gradient(rgb(49, 36, 80,0.363), rgba(0, 0, 0, 0.486)), url(${arrayImages[arrayImages.length -1]})`
+                img.style.backgroundImage = `linear-gradient(rgb(64 125 197 / 14%), rgb(27 21 21 / 80%)), url(${arrayImages[arrayImages.length -1]})`
                 contador = arrayImages.length -1
             }
         }else if(tgt == adelante){
             if(contador < arrayImages.length -1){
-                img.style.backgroundImage = `linear-gradient(rgb(49, 36, 80,0.363), rgba(0, 0, 0, 0.486)), url(${arrayImages[contador + 1]})`
+                img.style.backgroundImage = `linear-gradient(rgb(64 125 197 / 14%), rgb(27 21 21 / 80%)), url(${arrayImages[contador + 1]})`
                 contador++
             }else{
-                img.style.backgroundImage = `linear-gradient(rgb(49, 36, 80,0.363), rgba(0, 0, 0, 0.486)), url(${arrayImages[0]})`
+                img.style.backgroundImage = `linear-gradient(rgb(64 125 197 / 14%), rgb(27 21 21 / 80%)), url(${arrayImages[0]})`
                 contador = 0
             }
         } 
@@ -61,8 +66,21 @@ for(let i = 0; i<btnProydos.length; i++){
     })
 }
 
+function ocultarBotones(){
+    if(window.scrollY > 80){
+        btnOcultarNetx.classList.add('container__btn--ocultar') 
+        btnOcultarBack.classList.add('container__btn--ocultar')
+     }else if(window.scrollY === 0){
+        btnOcultarNetx.classList.remove('container__btn--ocultar') 
+        btnOcultarBack.classList.remove('container__btn--ocultar')
+    }
+}
+
+document.addEventListener('scroll', ocultarBotones)
+
 
 burger.addEventListener('click', visualizarLista)
+
 document.addEventListener('DOMContentLoaded', () => {
     const contenedor = document.querySelector('.container__inner')
     carrousel(contenedor)
